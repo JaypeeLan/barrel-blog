67cb7e16972d351d05d176ce323d9a788b5c95fc
Laniran JohnPaul <jplaniran01@gmail.com>
2024-02-01 10:54:43 +0100

    build fix

--- app/api/route.js
+++
@@ -1,6 +0,0 @@
-import { revalidatePath } from "next/cache";
-
-export default function handler(req, res) {
-  revalidatePath("/blog");
-  res.status(200).json({ message: "Data revalidation triggered" });
-}
